f808bbfe296dcd6b9f37a8213b25cd87030af789
Dominik Picheta <dominikpicheta@gmail.com>
2017-11-07 21:16:51 +0000

    First step towards generalizing httpbeast.

--- beast.nim
+++ beast.nim
@@ -1,49 +1,74 @@
-import selectors, net, nativesockets, os
+import selectors, net, nativesockets, os, httpcore, asyncdispatch, strutils
 
 from osproc import countProcessors
 
 import times # TODO this shouldn't be required. Nim bug?
 
+export httpcore.HttpMethod
+
 type
   Data = object
-    isServer: bool
+    isServer: bool ## Determines whether FD is the server listening socket.
+    ## A queue of data that needs to be sent when the FD becomes writeable.
     sendQueue: string
+    ## The number of characters in `sendQueue` that have been sent already.
     bytesSent: int
+    ## Big chunk of data read from client during request.
+    data: string
+    ## Determines whether `data` contains "\c\l\c\l"
+    headersFinished: bool
+
+type
+  Request* = object
+    selector: Selector[Data]
+    client: SocketHandle
+
+  OnRequest* = proc (req: Request) {.gcsafe.}
 
 proc initData(isServer: bool): Data =
   Data(isServer: isServer,
-       sendQueue: "HTTP/1.1 200 OK\c\LContent-Length: 11\c\L\c\LHello World",
-       bytesSent: 0)
+       sendQueue: "",
+       bytesSent: 0,
+       data: "",
+       headersFinished: false
+      )
+
+template handleAccept() =
+  let (client, address) = fd.SocketHandle.accept()
+  if client == osInvalidSocket:
+    let lastError = osLastError()
+    raiseOSError(lastError)
+  setBlocking(client, false)
+  selector.registerHandle(client, {Event.Read},
+                          initData(false))
+
+template handleClientClosure() =
+  # TODO: Logging that the socket was closed.
+  selector.unregister(fd)
+  fd.SocketHandle.close()
+  break
 
 proc processEvents(selector: Selector[Data],
-                   events: array[64, ReadyKey], count: int) =
+                   events: array[64, ReadyKey], count: int,
+                   onRequest: OnRequest) =
   for i in 0 .. <count:
     let fd = events[i].fd
-    var data = selector.getData(fd)
+    template data: var Data = selector.getData(fd)
     if data.isServer:
       if Event.Read in events[i].events:
-        let (client, address) = fd.SocketHandle.accept()
-        if client == osInvalidSocket:
-          let lastError = osLastError()
-          raiseOSError(lastError)
-        setBlocking(client, false)
-        selector.registerHandle(client, {Event.Read},
-                                initData(false))
+        handleAccept()
       else:
-        assert false
+        assert false, "Only Read events are expected for the server"
     else:
       if Event.Read in events[i].events:
         assert data.sendQueue.len == 0
-        # Read until EAGAIN. Hacky, but should work :)
         const size = 256
         var buf: array[size, char]
+        # Read until EAGAIN. Hacky, but should work :)
         while true:
           let ret = recv(fd.SocketHandle, addr buf[0], size, 0.cint)
           if ret == 0:
-            echo("Client closed")
-            selector.unregister(fd)
-            fd.SocketHandle.close()
-            break
+            handleClientClosure()
 
           if ret == -1:
             # Error!
@@ -51,16 +76,20 @@ proc processEvents(selector: Selector[Data],
             if lastError.int32 in {EWOULDBLOCK, EAGAIN}:
               break
             if isDisconnectionError({SocketFlag.SafeDisconn}, lastError):
-              echo("Client closed")
-              selector.unregister(fd)
-              fd.SocketHandle.close()
-              break
+              handleClientClosure()
             raiseOSError(lastError)
 
+          # Write buffer to our data.
+          data.data.add(addr(buf[0]))
+
           if buf[ret-1] == '\l' and buf[ret-2] == '\c':
-            # Request finished.
-            selector.updateHandle(fd.SocketHandle,
-                                  {Event.Read, Event.Write})
+            # First line and headers for request received.
+            # TODO: For now we only support GET requests.
+            # TODO: Check for POST and Content-Length in the most
+            # TODO: optimised way possible!
+            data.headersFinished = true
+            assert data.sendQueue.len == 0
+            onRequest(Request(selector: selector, client: fd.SocketHandle))
 
           if ret != size:
             # Assume there is nothing else for us and break.
@@ -77,20 +106,21 @@ proc processEvents(selector: Selector[Data],
           let lastError = osLastError()
           if lastError.int32 in {EWOULDBLOCK, EAGAIN}:
             break
+          if isDisconnectionError({SocketFlag.SafeDisconn}, lastError):
+            handleClientClosure()
           raiseOSError(lastError)
 
         data.bytesSent.inc(ret)
 
         if data.sendQueue.len == data.bytesSent:
           data.bytesSent = 0
+          data.sendQueue.setLen(0)
           selector.updateHandle(fd.SocketHandle,
                                 {Event.Read})
-
-        doAssert selector.setData(fd, data)
       else:
         assert false
 
-proc eventLoop() =
+proc eventLoop(onRequest: OnRequest) =
   let selector = newSelector[Data]()
 
   let server = newSocket()
@@ -104,14 +134,36 @@ proc eventLoop() =
   var events: array[64, ReadyKey]
   while true:
     let ret = selector.selectInto(-1, events)
-    processEvents(selector, events, ret)
+    processEvents(selector, events, ret, onRequest)
 
-let cores = countProcessors()
-if cores > 1:
-  echo("Starting ", cores, " threads")
-  var threads = newSeq[Thread[void]](cores)
-  for i in 0 .. <cores:
-    createThread[void](threads[i], eventLoop)
-  joinThreads(threads)
-else:
-  eventLoop()
+#[ API start ]#
+
+proc reqMethod*(req: Request): HttpMethod =
+  ## Parses the request's data to find the HttpMethod.
+  let fdData = req.selector.getData(req.client)
+  # fdData.
+
+proc send*(req: Request, body: string) =
+  template data: var Data = req.selector.getData(req.client)
+  assert data.headersFinished
+  data.sendQueue.add(
+    "HTTP/1.1 200 OK\c\LContent-Length: $1\c\L\c\L$2" % [$body.len, body]
+  )
+  req.selector.updateHandle(req.client, {Event.Read, Event.Write})
+
+proc run*(onRequest: OnRequest) =
+  let cores = countProcessors()
+  if cores > 1:
+    echo("Starting ", cores, " threads")
+    var threads = newSeq[Thread[OnRequest]](cores)
+    for i in 0 .. <cores:
+      createThread[OnRequest](threads[i], eventLoop, onRequest)
+    joinThreads(threads)
+  else:
+    eventLoop(onRequest)
+
+when isMainModule:
+  proc onRequest(req: Request) =
+    req.send("Hello World")
+
+  run(onRequest)
